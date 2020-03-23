a9b0d8910f5ff23f7fac703afcea49b644b943e6
dryuri92 <39188804+dryuri92@users.noreply.github.com>
2020-03-23 09:14:48 +0300

    code speed up
    some changes

--- hdf5/Table_Mend.py
+++ hdf5/Table_Mend.py
@@ -50,4 +50,34 @@ def determ_el(el):
     elif el[0:1] == '8':
         return 'O'
 
-
+def translaterlist(el):
+      elnuclides = ['O', 'Na', 'Mo', 'Mn', 'Zr', 'Mg', 'Al', 'Ca', 'Ti', 'V', 'Co', 'Cu', 'H', 'Si', 'C', 'B10', 'B11']
+      outelnuclides = ['O16', 'Na23', 'Mo0', 'Mn55',  'Zr0', 'Mg0', 'Al27', 'Ca0', 'Ti0', 'V0', 'Co59', 'Cu0', 'H1',  'Si0', 'C0', 'B10', 'B11']
+      excl = [ 'Fe', 'Cr', 'Ni', 'Nb', 'N', 'Sb']
+      if (el in elnuclides):
+          return [(outelnuclides[elnuclides.index(el)], 1.0)]
+      elif (el in excl):
+          e = []
+          ee = openmc.Element(el).expand(1, 'ao')
+          for v in ee:
+                e.append((v[0], v[1]))
+          return e
+      else:
+          return []
+#
+def get_unite_list(nuclist, ellist):
+    element_from = []
+    element_ind = []
+    element_val = []
+    for j, e in enumerate(ellist):
+        for ee in translaterlist(e):
+            if (ee[0] in nuclist):
+                element_from.append(j)
+                element_ind.append(nuclist.index(ee[0]))
+                element_val.append(ee[1])
+            else:
+                element_from.append(j)
+                nuclist.append(ee[0])
+                element_ind.append(len(nuclist) - 1)
+                element_val.append(ee[1])
+    return nuclist,element_from, element_ind, element_val
